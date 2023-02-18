ba528e4a767540d75fcf82f339a037001af61b22
san994 <75426313+san994@users.noreply.github.com>
2023-02-18 16:04:25 +0530

    Add files via upload

---
+++ Screens/Filter9.js
@@ -0,0 +1,45 @@
+import React from "react";
+import {Image,View} from "react-native";
+
+const Filter9=({
+    face:{
+        bounds:{
+            size:{
+                width:faceWidth,
+                height:faceHeight
+            }
+        },
+        LEFT_EYE,
+        RIGHT_EYE
+    }
+})=>{
+
+    const glassWidth = faceWidth;
+    const glassHeight = faceHeight/3;
+
+    const transformAngle =(angleRad = Math.atan((RIGHT_EYE.y-LEFT_EYE.y)/(RIGHT_EYE.x-LEFT_EYE.x)))=>{
+       angleRad*180/Math.PI
+    }
+
+    return(
+        <View style={{
+            position:"absolute",
+            left:LEFT_EYE.x-glassWidth*0.675,
+            top:LEFT_EYE.y-glassHeight*0.5
+        }}>
+            <Image 
+              source={require("../assets/Frapp-08.png")}
+              style={{
+                width:glassWidth,
+                height:glassHeight,
+                resizeMode:"contain",
+                transform:[{rotate:`${transformAngle()}deg`}]
+              }}
+            />
+        </View>
+    )
+}
+
+
+
+export default Filter9;
